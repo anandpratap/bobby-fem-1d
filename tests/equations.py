b36ffd14beefe86c469c5eb89f4f7c50c0e77ffe
Anand Pratap Singh <apsingh@altair.com>
2016-07-31 21:15:49 -0700

    added test cases for all equations

--- tests/equations.py
+++ tests/equations.py
@@ -5,35 +5,38 @@ import numpy.testing as npt
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
-from src.equations import EulerEquation
+from src.equations import Equation, BurgersEquation, SystemEquation, EulerEquation
 
 class TestGaussianQuadrature(unittest.TestCase):
-    def test_jacobian_integral(self):
-        equation = EulerEquation()
+    def test_jacobian(self):
+        for equation in [Equation, BurgersEquation, SystemEquation, EulerEquation]:
+            self.jacobian(equation())
+
+    def jacobian(self, eqn):
         eps = 1e-12
-        u = np.ones(3, dtype=np.complex)
-        dRdQ_approx_complex = np.zeros([3, 3])
-        dRdQ_approx_fd = np.zeros([3, 3])
-        dRdQ_exact = np.zeros([3, 3])
+        u = np.ones(eqn.nvar, dtype=np.complex)
+        dRdQ_approx_complex = np.zeros([eqn.nvar, eqn.nvar])
+        dRdQ_approx_fd = np.zeros([eqn.nvar, eqn.nvar])
+        dRdQ_exact = np.zeros([eqn.nvar, eqn.nvar])
 
 
-        for i in range(3):
-            for j in range(3):
-                dRdQ_exact[i,j] = equation.dflux[i][j](u).astype(np.float64)
+        for i in range(eqn.nvar):
+            for j in range(eqn.nvar):
+                dRdQ_exact[i,j] = eqn.dflux[i][j](u).astype(np.float64)
 
-        R = np.array([equation.flux[i](u) for i in range(3)])
-        for i in range(3):
+        R = np.array([eqn.flux[i](u) for i in range(eqn.nvar)])
+        for i in range(eqn.nvar):
             u[i] += eps*1j
-            for j in range(3):
-                dRdQ_approx_complex[j,i] = np.imag(equation.flux[j](u))/eps
+            for j in range(eqn.nvar):
+                dRdQ_approx_complex[j,i] = np.imag(eqn.flux[j](u))/eps
             u[i] -= eps*1j
 
             
         eps = 1e-6
-        for i in range(3):
+        for i in range(eqn.nvar):
             u[i] += eps
-            for j in range(3):
-                R_p = np.array([equation.flux[k](u) for k in range(3)])
+            for j in range(eqn.nvar):
+                R_p = np.array([eqn.flux[k](u) for k in range(eqn.nvar)])
                 dRdQ_approx_fd[j,i] = np.real((-R[j] + R_p[j])/eps)
             u[i] -= eps
             
